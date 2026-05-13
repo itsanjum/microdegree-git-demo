140622098ac58a8ecb4f1b7bf70c92185cef6356
Anjum Unnisa <anjumunnisa1999@gmail.com>
2026-05-13 21:40:25 +0530

    pushing multiple files..

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
 print("Hello Anjum")
 print("Welcome to Microdegree Git Demo")
+print("This is a sample code to demonstrate git operations")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Just want to showcase multiple file edits")
 
 print("Hi there, just pushing some new code")
+print("This is the main.py file, which is different from app.py")
